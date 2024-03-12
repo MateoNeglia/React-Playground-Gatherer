e09358c8d5433235a1f5814a2128bacd5de2887b
MateoNeglia <mateo.neglia@gmail.com>
2024-03-12 14:08:17 -0300

    playground gatherer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+let cardList = [
+  {
+    name: 'Pokemon Game API',
+    image: 'images/pokemon-api.png',
+    url: 'https://pokemon-m7mv78mpx-mateoneglia.vercel.app/',
+  },
+  {
+    name: 'Color Palette Picker',
+    image: 'images/color-palette.png',
+    url: 'https://color-palettes-app.vercel.app/'
+  },
+  {
+    name: 'Card-dealing API',
+    image: 'images/card-deal.png',
+    url: 'https://react-card-dealing-app.vercel.app/'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+        <h1>This are some of my current React playground apps</h1>
+        <p>I've made for testing and practicing purposes</p>
+        
+        <div>
+        {cardList.map((project, index) => (
+          <div className='project-card image-box' key={index}>
+            <a href={project.url} target='_blank' rel="noreferrer" >
+              <img className='portfolio-image' src={project.image} alt={project.name} width={'300px'}/>
+              <div className='content'>
+                <h3 className='title'>{project.name}</h3>
+              </div>            
+            </a>            
+          </div>            
+        ))}
+        </div>
     </div>
   );
 }
